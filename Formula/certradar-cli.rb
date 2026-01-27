be4b0fa59eda0e90d067939e61dcf462dd5e1a94
Alex Fler <alexfler@msn.com>
2026-01-26 20:29:11 -0500

    Update certradar-cli to v0.1.0 with correct SHA256

--- Formula/certradar-cli.rb
+++ Formula/certradar-cli.rb
@@ -5,18 +5,15 @@ class CertradarCli < Formula
     license "MIT"
 
     on_macos do
-      if Hardware::CPU.arm?
+      on_arm do
         url "https://github.com/FlerAlex/certradar-cli/releases/download/v0.1.0/certradar-cli-macos-arm64.tar.gz"
-        sha256 "REPLACE_WITH_ACTUAL_SHA256"
-      else
-        url "https://github.com/FlerAlex/certradar-cli/releases/download/v0.1.0/certradar-cli-macos-x86_64.tar.gz"
-        sha256 "REPLACE_WITH_ACTUAL_SHA256"
+        sha256 "afbe65ca9657777c122d223271a5dcbc5a0a0cf376847b69a4cc084d59722f97"
       end
     end
 
     on_linux do
       url "https://github.com/FlerAlex/certradar-cli/releases/download/v0.1.0/certradar-cli-linux-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      sha256 "c4a87dfbb7ae250811e5ec40ef264fea3f6a868dd29d7e90093c5f513dbce702"
     end
 
     def install
